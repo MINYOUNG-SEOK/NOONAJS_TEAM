8630f4a038f363a31813beca30cb219e7cd25a23
Jaeework <jaeework@gmail.com>
2025-03-09 02:11:38 +0900

    feat: 즐겨찾기 기능 개선 및 페이지네이션 추가
    - 팝업 알림창 기능 완성
    - 페이지네이션 기능 추가 및 ui 구현
    - 렌더링 함수에 페이지네이션 관련 로직 추가

--- pages/favorites/favorites.js
+++ pages/favorites/favorites.js
@@ -8,7 +8,6 @@ const groupSize = 5;
 const getLikedItems = () => {
     const likedItems = localStorage.getItem("likedItems");
     likedItemsList = likedItems ? JSON.parse(likedItems) : []; 
-
     if(likedItemsList.length > 0) {
         console.log('likedItem', likedItemsList);
 
@@ -16,15 +15,20 @@ const getLikedItems = () => {
         renderLikedItems();
         renderPagination();
     } else {
-
+        renderEmptyLikedItems();
     }
 }
 
 const renderLikedItems = () => {
-    let booksHTML = likedItemsList.map( book => `
+    // 페이지네이션 적용을 위해 시작, 끝 인덱스 추가
+    const startIndex = (page - 1) * pageSize;
+    const endIndex = Math.min(startIndex + pageSize, likedItemsList.length);
+    const currentPageItems = likedItemsList.slice(startIndex, endIndex);
+
+    let booksHTML = currentPageItems.map( book => `
         <div class="fav_book-card">
             <div class="fav_book-thum">
-                <button type="button" class="fav_button-like" onclick="removeLike(${book.itemId})">
+                <button type="button" class="fav_button-like" onclick="openLikeRemovePopup(${book.itemId})">
                     <i class="fa fa-solid fa-heart"></i>
                     <span class="fav_blind">찜 해제</span>
                 </button>
@@ -41,6 +45,12 @@ const renderLikedItems = () => {
     document.getElementById('book-list').innerHTML = booksHTML;
 }
 
+const renderEmptyLikedItems = () => {
+    let booksHTML = '<div class="fav_no-items">좋아하는 도서를 추가해 보세요</div>'
+
+    document.getElementById('book-list').innerHTML = booksHTML;
+}
+
 const renderPagination = () => {
     const totalPages = Math.ceil(totalResults / pageSize);
     const pageGroup = Math.ceil(page / groupSize);
@@ -51,38 +61,62 @@ const renderPagination = () => {
 
     let paginationHTML = ``;
 
+    //  처음 페이지 (`«`) 버튼 (첫 페이지에서는 숨김)
+    if (page > 1) {
+        paginationHTML = `<li class="page-item" onclick="moveToPage(1)">
+                            <a class="page-link">«</a></li>
+        <li class="page-item" onclick="moveToPage(${page - 1})">
+                            <a class="page-link">‹</a></li>`;
+    }
+
+    for (let i = firstPage; i <= lastPage; i++) {
+        paginationHTML += `<li class="page-item ${i === page ? 'active' : ''}" onclick="moveToPage(${i})"><a class="page-link">${i}</a></li>`;
+    }
+
+    //  다음 페이지 (`›`) 버튼 (마지막 페이지에서는 숨김)
+    if (page < totalPages) {
+        paginationHTML += `<li class="page-item" onclick="moveToPage(${page + 1})">
+                            <a class="page-link">›</a></li><li class="page-item" onclick="moveToPage(${totalPages})">
+                            <a class="page-link">»</a></li>`;
+    }
+
     
+    document.querySelector(".pagination").innerHTML = paginationHTML;
 }
 
 const removeLike = (itemId) => {
 
-    openPopup('즐겨찾기 목록에서 제외하시겠습니까?');
-    // const index = likedItemsList.findIndex( item => item.itemId === itemId);
-    // if(index > -1) {
-    //     likedItemsList.splice(index, 1);
-    //     localStorage.setItem('likedItems', JSON.stringify(likedItemsList));
+    const index = likedItemsList.findIndex( item => item.itemId === itemId);
+    if(index > -1) {
+        likedItemsList.splice(index, 1);
+        localStorage.setItem('likedItems', JSON.stringify(likedItemsList));
 
-    //     renderLikedItems();
-    // }
-    
+        // 총 결과 수 업데이트
+        totalResults = likedItemsList.length;
+        // 현재 페이지 아이템이 0개가 되면 이전 페이지로 이동, 1페이지 제외
+        const totalPages = Math.ceil(totalResults / pageSize);
+        if(page > totalPages && page > 1) {
+            page = totalPages;
+        }
+
+        renderLikedItems();
+        renderPagination();
+    }
 }
 
-const moveToDetail = (itemId) => {
-    location.href = `/pages/book-detail/detail?itemId=${itemId}`;
-}
-
-const openPopup = (message) => {
-    document.querySelector('.fav_alert-msg').innerHTML = message;
+const openLikeRemovePopup = (itemId) => {
+    document.querySelector('.fav_alert-msg').innerHTML = '즐겨찾기 목록에서 제외하시겠습니까?';
     document.querySelector('.fav_system-alert').style.display = 'block';
 
     document.querySelectorAll('.fav_pop-foot-button').forEach((button) => {
         const spanText = button.querySelector('span').textContent;
-        button.addEventListener("click", () => closePopup(spanText));
+        button.addEventListener("click", () => closePopup(spanText, itemId));
     });
 }
 
-const closePopup = (type) => {
+const closePopup = (type, itemId) => {
     if(type === '확인') {
+        removeLike(itemId);
         document.querySelector('.fav_alert-msg').innerHTML = '';
         document.querySelector('.fav_system-alert').style.display = 'none';
     } else {
@@ -91,7 +125,15 @@ const closePopup = (type) => {
     }
 }
 
+const moveToDetail = (itemId) => {
+    location.href = `/pages/book-detail/detail?itemId=${itemId}`;
+}
 
-
+const moveToPage = (pageNum) => {
+    console.log("moveToPage", pageNum);
+    page = pageNum;
+    renderLikedItems();
+    renderPagination();
+}
 
 getLikedItems();
